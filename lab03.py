cebe80e20255811e2dd5e5eb807390bc6637278d
cheatbreak <artembaskoca@gmail.com>
2025-01-24 14:04:50 -0500

    Finished lab3

--- lab03.py
+++ lab03.py
@@ -1,3 +1,4 @@
+import random
 #Dice Options created using list() and range()
 diceOptions = list(range(1,7))
 
@@ -10,10 +11,27 @@ print("Available weapons are: ", ",".join(weapons))
 def getCombatStrength(prompt):
     while True:
         value = int(input(prompt))
-        if 1 <= value <= 60:
+        if 1 <= value <= 6:
             return value
         else:
             print("Invalid Input! Please enter a number between 1-6")
 
-combatStrength = getCombatStrength("Please enter a nuber between 1-6 for player")
-mCombatStrength = getCombatStrength("Please enter a nuber between 1-6 for player")
+combatStrength = getCombatStrength("Please enter a number between 1-6 for player")
+mCombatStrength = getCombatStrength("Please enter a number between 1-6 for player")
+
+for j in range(1, 21, 2):
+    heroRoll = random.choice(diceOptions)
+    monsterRoll = random.choice(diceOptions)
+
+    heroWeapon = weapons(heroRoll - 1)
+    monsterWeapon = weapons(monsterRoll - 1)
+
+    heroTotal = combatStrength + heroRoll
+    monsterTotal = mCombatStrength + monsterRoll
+
+    if heroTotal > monsterTotal:
+        print("Player wins!")
+    elif heroTotal < monsterTotal:
+        print("Monster wins!")
+    else:
+        print("Its a tie!")
